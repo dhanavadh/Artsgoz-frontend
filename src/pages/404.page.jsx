5d5c02db0a1358cd2b6db8966e5e264b3b863d4a
Dhana <105502355+dhanavadh@users.noreply.github.com>
2024-06-25 19:32:10 +0700

    edit package json
    edit package json

--- src/pages/404.page.jsx
+++ src/pages/404.page.jsx
@@ -13,10 +13,10 @@ const PageNotFound = () => {
     return (
         <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">
 
-            <img src={ theme == "light" ? darkPageNotFoundImg : lightPageNotFoundImg } className="select-none border-2 border-grey w-72 aspect-square object-cover rounded" />
+            {/* <img src={ theme == "light" ? darkPageNotFoundImg : lightPageNotFoundImg } className="select-none border-2 border-grey w-72 aspect-square object-cover rounded" /> */}
 
-            <h1 className="text-4xl font-gelasio leading-7">Page not found</h1>
-            <p className="text-dark-grey text-xl leading-7 -mt-8">The page you are looking for does not exists. Head back to the <Link to="/" className="text-black underline">home page</Link></p>
+            <h1 className="text-4xl font-gelasio leading-7">ไม่พบหน้า</h1>
+            <p className="text-dark-grey text-xl leading-7 -mt-8">ไม่พบหน้าที่คุณกำลังตามหา กำลับไปยัง <Link to="/" className="text-black underline">หน้าแรก</Link></p>
 
             <div className="mt-auto">
                 <img src={ theme == "light" ? darkFullLogo : lightFullLogo } className="h-8 object-contain block mx-auto select-none" />
