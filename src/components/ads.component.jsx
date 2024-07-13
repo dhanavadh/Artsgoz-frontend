375e6351517209b6b05defe7754b32fcb0d25273
Dhana <105502355+dhanavadh@users.noreply.github.com>
2024-07-13 19:10:41 +0700

    Update ads.component.jsx
    fix lg font size

--- src/components/ads.component.jsx
+++ src/components/ads.component.jsx
@@ -7,8 +7,8 @@ const Ads = () => {
         <div className=''>
                 <div className='flex max-h-96 relative'>
                     <img src={frame4} className='rounded-xl h-96 absolute object-cover -z-[1] brightness-50'></img>                        
-                        <div className='grid grid-rows mx-10 mt-32'>
-                            <p className='grid text-[32px] text-[#F8E1EA] font-kanit'>We are looking for Developers! <br /> Join us?</p>
+                        <div className='grid grid-rows mx-8 mt-32'>
+                            <p className='grid text-[26px] lg:text-[32px] text-[#F8E1EA] font-kanit'>We are looking for Developers! <br /> Join us?</p>
                             <div className='grid'>
                                     <Link to='https://github.com/artsgoz' target='_blank' type='button' className='flex px-2 py-2 border-2 rounded-lg border-[#F8E1EA] w-fit h-12 mt-4 text-[#F8E1EA] text-xl hover:bg-[#F8E1EA] hover:text-[#1F1F1F]
             `                           cursor-pointer transition-colors duration-300'>
