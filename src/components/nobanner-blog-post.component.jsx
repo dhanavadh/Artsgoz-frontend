0a1eaba69e0417282643fe0e522662d524847f63
Dhana <105502355+dhanavadh@users.noreply.github.com>
2024-07-13 21:45:40 +0700

    update trending blogs panel
    update trending blogs panel

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -4,20 +4,21 @@ import { getDay } from "../common/date";
 
 const MinimalBlogPost = ({ blog, index }) => {
     
-    let { title, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+    let { title, blog_id: id, author: { personal_info: { username, profile_img } }, publishedAt } = blog;
 
     return (
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
             <p className="blog-index">{ index < 10 ? "0" + (index + 1) : index}</p>
 
             <div>
-                <div className="flex gap-2 items-center mb-7">
-                    <img src={profile_img} className="w-6 h-6 rounded-full" />
-                    <p className="line-clamp-1">{fullname} @{username}</p>
-                    <p className="min-w-fit">{ getDay(publishedAt) }</p>
-                </div>
+                
 
                 <h1 className="blog-title">{title}</h1>
+                <div className="flex gap-2 items-center mt-3">
+                    <img src={profile_img} className="w-6 h-6 rounded-full" />
+                    <p className="line-clamp-1">@{username}</p>
+                    <p className="min-w-fit">โพสต์เมื่อ { getDay(publishedAt) }</p>
+                </div>
             </div>
         </Link>
     )
