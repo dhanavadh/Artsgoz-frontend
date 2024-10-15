1bca19121c235ca6d9752a675d5e95362dac0bcc
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2024-10-15 15:31:51 +0700

    added under construct page
    added under construct page

---
+++ src/pages/construction.page.jsx
@@ -0,0 +1,10 @@
+const PageConstruct = () => {
+    return (
+        <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">                        
+            <h1 className="text-4xl leading-7">กำลังปรับปรุงหน้านี้</h1>
+            <p className="text-dark-grey text-xl leading-7 -mt-8">ขออภัยในความไม่สะดวก หน้านี้กำลังปรับปรุงอยู่</p>
+        </section>
+    )
+}
+
+export default PageConstruct;
