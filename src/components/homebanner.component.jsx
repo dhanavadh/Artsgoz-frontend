ffb06c8f27d8409741312d8343fd705f9a2ab029
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2024-10-16 20:14:18 +0700

    change banner

--- src/components/homebanner.component.jsx
+++ src/components/homebanner.component.jsx
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 function HomeBanner() {
     return (
         <div>
-            <div className='mx-4 sm:mx-16 mt-5 mb-32'>
+            <div className='mx-4 sm:mx-16 mt-5 mb-16 md:mb-32'>
                 <div className='flex max-h-96 relative'>
                     <img src='https://www.arts.chula.ac.th/goz/asset/banner/8dCOiId6lJaxhgVS7UjcEjGy4mpFI4Zg.jpg' className='rounded-xl h-96 absolute object-cover -z-[1] brightness-50'></img>                        
-                        <div className='grid grid-rows mx-10 mt-32'>
-                            <p className='grid text-[40px] text-[#F8E1EA] font-kanit'> ข้อมูลสำหรับนิสิตใหม่ที่ควรรู้</p>
+                        <div className='grid grid-rows mx-10 mt-16 md:mt-32'>
+                            <p className='grid text-[40px] text-[#F8E1EA] font-kanit'>อยากรู้รีวิวฝึกงานแต่ละที่เป็นอย่างไร?</p>
+                            <p className='grid text-[20px] text-[#F8E1EA] font-normal'>คณะอักษรฯ มีฝึกงานด้วยหรือ ข้อมูลเกี่ยวกับการฝึกงานอยู่ที่นี่หมดแล้ว รอติดตามเร็ว ๆ นี้</p>
                             <div className='grid'>
-                                    <Link to='https://sites.google.com/view/regartscu/' target='_blank' type='button' className='flex px-2 py-2 border-2 rounded-lg border-[#F8E1EA] w-fit h-12 mt-4 text-[#F8E1EA] text-xl underline-thickness-1 hover:underline hover:bg-[#F8E1EA] hover:text-[#1F1F1F]
+                                    <Link to='/intern' target='_blank' type='button' className='flex px-2 py-2 border-2 rounded-lg border-[#F8E1EA] w-fit h-12 mt-4 text-[#F8E1EA] text-xl underline-thickness-1 hover:underline hover:bg-[#F8E1EA] hover:text-[#1F1F1F]
             `                           cursor-pointer transition-colors duration-300'>
                                         เรียนรู้เพิ่มเติม                         
                                     </Link>
